55c341891713c5692c2dbf5ec791c77e9b4cc4d2
NAZIM MERT BİLGİ <nmbjobs@outlook.com.tr>
2024-11-14 23:12:07 +0300

    geliştirmeler..

--- assets/js/home/home.js
+++ assets/js/home/home.js
@@ -1,13 +1,13 @@
 const helloWorld = "Hello World!";
 const firstImg = "assets/img/cat.jpeg";
-const calculate = (2*5);
+const calculate = (2 * 5);
 const calculateText = "2 * 5";
 
 changeHelloWorldText = () => {
-    window.helloWorld.value = "Hello World! Changed!";
+    window.helloWorld.value = "Hello World! Changed!"
     window.firstImg.value = "assets/img/clouds.jpg";
     window.calculateText.value = "3 * 5";
-    window.calculate.value = (3*5);
+    window.calculate.value = (3 * 5);
 }
 
 document.addEventListener('nmbInteractiveLoaded', function (e) {
